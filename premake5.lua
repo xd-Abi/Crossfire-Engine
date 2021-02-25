827c63cfc6a19557514644585f5a1a8d752671f3
xdAbitur <abinayansureskumar@gmail.com>
2021-02-25 22:08:45 +0100

    Setup project using premake5

---
+++ premake5.lua
@@ -0,0 +1,64 @@
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+bin = "%{wks.location}/bin/" .. outputdir .. "/%{prj.name}"
+binint = "%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}"
+
+workspace "Crossfire"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+include "Crossfire"
+
+project "Sandbox"
+location "Sandbox"
+kind "ConsoleApp"
+language "C++"
+
+targetdir (bin)
+objdir (binint)
+
+files
+{
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+}
+
+includedirs
+{
+    "Crossfire/src"
+}
+
+links
+{
+    "Crossfire"
+}
+
+filter "system:windows"
+    cppdialect "C++17"
+    staticruntime "On"
+    systemversion "latest"
+
+    defines
+    {
+        "CF_PLATFORM_WINDOWS"
+    }
+
+filter "configurations:Debug"
+    defines "CF_DEBUG"
+    buildoptions "/MDd"
+    symbols "On"
+
+filter "configurations:Release"
+    defines "CF_RELEASE"
+    buildoptions "/MD"
+    optimize "On"
+
+filter "configurations:Dist"
+    defines "CF_DIST"
+    buildoptions "/MD"
+    optimize "On" 
